0213b23dddbcc8dfd999c716be86b1ee2571678d
jmartinez <jcmartinez1925@gmail.com>
2019-06-28 23:39:08 -0700

    revise functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var countryList = ["Andorra", "Azerbaijan", "Bahrain", "Bhutan", "Botswana", "Chad", "Comoros", "Djibouti", "Dominica", "Eritrea", "Eswatini", "Gabon", "Latvia", "Madagascar", "Moldova", "Rwanda", "Singapore", "Suriname", "Tanzania", "Zambia"]
+var countryList = ["andorra", "azerbaijan", "bahrain", "bhutan", "botswana", "chad", "comoros", "djibouti", "dominica", "eritrea", "eswatini", "gabon", "latvia", "madagascar", "moldova", "rwanda", "singapore", "suriname", "tanzania", "zambia"]
 
 //Create variables to hold references in the HTML
 var directionsText = document.getElementById("directions-text");
@@ -7,50 +7,102 @@ var currentWord = document.getElementById("current-word");
 var remainingGuesses = document.getElementById("remaining-guesses");
 var lettersGuessed = document.getElementById("letters-guessed");
 
-//Number of tries
-const maxTries = 10;
 
-//Create wins variable
-var wins = 0;
+//Set up variables
+const maxTries = 10
 
-//Set up variable to show how many remaining guesses
-var remainingGuesses = countryList.length;
+var guessedLetters = []
+var guessedWord = []
+var currentWord
+var remainingGuesses
+var pauseGame = false
+var wins = 0
 
-//Set up guessArray to show how many letters in the country name with an underscore
-var guessArray = [];
+resetGame()
 
-for (var i = 0; i < countryList.length; i++) {
-    guessArray[i] = "_";
+//Set up key function
+document.onkeypress = function (event) {
+
+    if (isAlpha(event.key) && !pauseGame) {
+        checkForLetter(event.key.toUpperCase())
+    }
 }
 
-//Set up variable to for index of current letter in array
-var currentLetterIndex
+function checkForLetter(letter) {
+    var foundLetter = false
 
-//Pick random country
-var randomCountry = countryList[Math.floor(Math.random() * countryList.length)];
+//Check for correctt/incorrect guesses
+    for (var i = 0, j = currentWord.length; i < j; i++) {
+        if (letter === currentWord[i]) {
+            guessedWord[i] = letter
+            foundLetter = true
 
-//Set up game event
-document.onkeyup = function (event) {
-    var userGuess = event.key;
-
-    //Set up logic of the game
-   
-
-
-    //Update game with the guess
-    for (var j = 0; j < countryList; j++) {
-        if (countryList[j] === userGuess) {
-            guessArray[j] = userGuess;
-            remainingGuesses--;
+            if (guessedWord.join("") === currentWord) {
+                wins++
+                pauseGame = true
+                updateDisplay()
+                setTimeout(resetGame, 5000)
+            }
         }
     }
 
-    //Set up displays
-    directionsText.textContent = "Press letter key to start playing!";
-    winsText.textContent = "Wins: " + wins;
-    currentWord.textContent = randomCountry;
-    remainingGuesses.textContent = "Remaining Guesses: " + guessArray;
-    lettersGuessed.textContent = lettersGuessed;
+    if (!foundLetter) {
+            if (!guessedLetters.includes(letter)) {
+            guessedLetters.push(letter)
+            remainingGuesses--
+        }
+        if (remainingGuesses === 0) {
+            guessedWord = currentWord.split()
+            pauseGame = true
+            setTimeout(resetGame, 5000)
+        }
+    }
+
+    updateDisplay()
+
+}
+
+
+function resetGame() {
+    numGuess = maxGuess
+    pauseGame = false
+
+    // Get a new word
+    wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+    console.log(wordToMatch)
+
+    // Reset word arrays
+    guessedLetters = []
+    guessingWord = []
+
+    // Reset the guessed word
+    for (var i = 0, j = wordToMatch.length; i < j; i++) {
+        // Put a space instead of an underscore between multi word "words"
+        if (wordToMatch[i] === " ") {
+            guessingWord.push(" ")
+        } else {
+            guessingWord.push("_")
+        }
+    }
+
+    // Update the Display
+    updateDisplay()
+}
+
+function updateDisplay() {
+    document.getElementById("totalWins").innerText = wins
+    document.getElementById("currentWord").innerText = guessingWord.join("")
+    document.getElementById("remainingGuesses").innerText = numGuess
+    document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+}
+})
+
+//Set up displays
+directionsText.textContent = "Press letter key to start playing!";
+winsText.textContent = "Wins: " + wins;
+currentWord.textContent = randomCountry;
+remainingGuesses.textContent = "Remaining Guesses: " + guessArray;
+lettersGuessed.textContent = lettersGuessed;
 
 }
 
