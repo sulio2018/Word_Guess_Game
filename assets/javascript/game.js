b05f44ff49cf17df8b708f280ad42dd23a0729c5
jmartinez <jcmartinez1925@gmail.com>
2019-06-29 21:09:51 -0700

    revisited loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -65,6 +65,7 @@ function checkLetters(letter) {
     }
 }
 
+
 //Update a full round of a game
 function fullRoundGame() {
 
@@ -74,7 +75,7 @@ function fullRoundGame() {
 
     if (currentLetters.toString() == answerArray.toString()) {
         wins++;
-        alert("CONTRATULATIONS! You guessed '" + currentWord + "' correctly. Try another word?");
+        alert("CONTRATULATIONS! You guessed '" + currentWord + "' correctly. Try another word.");
         document.getElementById("wins-text").innerHTML = "Wins: " + " " + wins;
 
         //Start New Game
